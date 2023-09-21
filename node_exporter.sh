02ba0cdf848dcfa201eeaf997cddbd6e1e5d93ab
Narendrakaduru <kadurunarendra@gmail.com>
2023-09-21 08:30:58 +0530

    Update node_exporter.sh

--- node_exporter.sh
+++ node_exporter.sh
@@ -37,6 +37,5 @@ EOL
 sudo systemctl daemon-reload
 sudo systemctl enable node_exporter
 sudo systemctl start node_exporter
-sudo systemctl status node_exporter
 sudo ufw allow 9100
 ###############  END  ###############
