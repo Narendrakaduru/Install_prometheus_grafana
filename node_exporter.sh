881ae119ecfc5712bb9d3b10212a00011ad5b1c1
Narendrakaduru <kadurunarendra@gmail.com>
2023-09-21 08:19:12 +0530

    Update node_exporter.sh

--- node_exporter.sh
+++ node_exporter.sh
@@ -8,10 +8,10 @@ sudo useradd --no-create-home --shell /bin/false node_exporter
 cd /opt
 sudo wget https://github.com/prometheus/node_exporter/releases/download/v1.6.1/node_exporter-1.6.1.linux-amd64.tar.gz
 sudo tar xvzf node_exporter-1.6.1.linux-amd64.tar.gz
-cd node_exporter-1.6.1.linux-amd64
-sudo cp node_exporter /usr/local/bin
+cd /opt/node_exporter-1.6.1.linux-amd64
+sudo cp /opt/node_exporter-1.6.1.linux-amd64/node_exporter /usr/local/bin
 
-sudo cat <<EOL | sudo tee /etc/systemd/system/node_exporter.service
+sudo cat <<EOL | sudo tee /lib/systemd/system/node_exporter.service
 [Unit]
 Description=Node Exporter
 Wants=network-online.target
@@ -21,13 +21,13 @@ Type=simple
 User=node_exporter
 Group=node_exporter
 ExecStart=/usr/local/bin/node_exporter \
-— collector.mountstats \
-— collector.logind \
-— collector.processes \
-— collector.ntp \
-— collector.systemd \
-— collector.tcpstat \
-— collector.wifi
+	--collector.mountstats \
+	--collector.logind \
+	--collector.processes \
+	--collector.ntp \
+	--collector.systemd \
+	--collector.tcpstat \
+	--collector.wifi
 Restart=always
 RestartSec=10s
 [Install]
@@ -37,5 +37,6 @@ EOL
 sudo systemctl daemon-reload
 sudo systemctl enable node_exporter
 sudo systemctl start node_exporter
+sudo systemctl status node_exporter
 sudo ufw allow 9100
 ###############  END  ###############
